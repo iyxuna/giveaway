1cdbd613e6bac257d431f24245170ac471ab8772
iyxuna <tjddbsk99@naver.com>
2022-08-19 14:57:44 +0900

    스크래치 에러 커밋

--- pages/success.js
+++ pages/success.js
@@ -1,9 +1,9 @@
 import React, {useEffect} from 'react';
 import Link from "next/link";
 import { useRouter } from 'next/router';
-import ScratchCard from "react-scratchcard-v2";
-// import IMG from "/images/img.png";
-import Image from "next/image";
+import ScratchCard from "react-scratchcard";
+
+// import Image from "./img.png";
 
 const Success = props => {
     const router = useRouter();
@@ -16,7 +16,7 @@ const Success = props => {
     const settings = {
         width: 320,
         height: 420,
-        image: "/images/img.png",
+        image: ("/images/img.png"),
         finishPercent: 50
     }
 
@@ -36,14 +36,15 @@ const Success = props => {
                         </header>
 
                         <div className={"inner result"}>
-                            {/*<ScratchCard source={require("/images/img.png")}>*/}
+                            <ScratchCard {...settings}>
                                 <div className={"scratch_wrap"}>
+                                {/*<Image src={"/images/img.png"} width={320} height={420} />*/}
                                     <div className={"img_wrap"}>
                                         <img src={"/images/success.jpg"} />
                                     </div>
                                     <button className={"main_btn"} onClick={e=>{handleShare(e)}}>자랑하기</button>
                                 </div>
-                            {/*</ScratchCard>*/}
+                            </ScratchCard>
                         </div>
 
                     </div>
@@ -58,7 +59,6 @@ export const getServerSideProps = async ctx=>{
 
     return{
         props : {
-
         }
     }
 }
